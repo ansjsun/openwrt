2b4895cf11214a4658c4cd6bb663014d9ba12fbb
sunjian126 <sunjian11@jd.com>
2023-02-14 12:22:37 +0800

    up

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,32 +17,32 @@
 # sed -i "s/127.0.0.1/127.0.0.1:5335/g"   package/luci-app-mosdns/root/etc/mosdns/set.sh
 
 # 添加SSR-Plus
-rm -rf package/luci-app-ssr-plus
-git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
+# rm -rf package/luci-app-ssr-plus
+# git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
 
 
 
 
 # 添加PassWall
-rm -rf package/passwall
-rm -rf package/luci-app-passwall
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
-git clone  https://github.com/xiaorouji/openwrt-passwall.git package/passwall
-mv -f package/passwall package/luci-app-passwall
+# rm -rf package/passwall
+# rm -rf package/luci-app-passwall
+# git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+# git clone  https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+# mv -f package/passwall package/luci-app-passwall
 
 
 
-git clone https://github.com/QiuSimons/openwrt-mos package/mosdns
+# git clone https://github.com/QiuSimons/openwrt-mos package/mosdns
 # git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 
 
 #adguardhome
-git clone https://github.com/limi00/luci-app-adguardhome.git package/luci-app-adguardhome
+# git clone https://github.com/limi00/luci-app-adguardhome.git package/luci-app-adguardhome
 
 
-git clone https://github.com/pymumu/openwrt-smartdns package/lean/smartdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
+# git clone https://github.com/pymumu/openwrt-smartdns package/lean/smartdns
+# git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
 
 
 
@@ -69,5 +69,4 @@ CONFIG_PACKAGE_libreadline=y
 CONFIG_PACKAGE_terminfo=y
 EOF
 cd ..
-mv -f msd_lite/ openwrt/package/msd_lite
 rm -rf package-temp
